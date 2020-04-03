5afbd60faca2c63d78960cd6e0ee24b6d01c9e75
datomezz <datomezz@gmail.com>
2020-04-03 16:11:12 +0400

    add stylecolor to carts and startingwork on seting mainc cart

--- games/Durak/script.js
+++ games/Durak/script.js
@@ -5,11 +5,24 @@ let origin = [
     '31','32','33','34','35','36','37','38','39',
     '41','42','43','44','45','46','47','48','49'
 ]
-console.log(origin);
+
 for(let i = 0; i < carts.length; i++){
     let rand = Math.floor(Math.random() * origin.length);
     carts[i].innerText = origin[rand];
 
+    let switchNumber = origin[rand].charAt(0);
+    switchNumber = Number(switchNumber);
+
+    switch(switchNumber){
+        case 1 : carts[i].style.background = "pink";
+        break;
+        case 2 : carts[i].style.background = "tomato";
+        break;
+        case 3 : carts[i].style.background = "lightgreen";
+        break;
+        case 4 : carts[i].style.background = "lightblue";
+        break;
+    }
     origin.splice(rand, 1);
 
     console.log(origin);
