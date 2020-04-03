195d7d99a96dc38db3843ec056fadffa218e7cb1
datomezz <datomezz@gmail.com>
2020-04-03 15:40:01 +0400

    started work on Durak game

---
+++ php/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mail Form</title>
+</head>
+
+<body>
+
+<script>
+    console.log("%cMEZZ.CODES %c\nFkn.. MEZZ.CODES .","font-size: 2rem; color: yellow; text-shadow: 1px 1px 1px black;","font-size: 12px;");
+</script>
+</body>
+
+</html>
