1fbdffc727165c8849bf070cd6aab037d8a0ca72
datomezz <datomezz@gmail.com>
2020-04-26 05:57:50 +0400

    do lot of shit with portfolio

---
+++ portfolio/Front-end/scipt.js
@@ -0,0 +1,17 @@
+AOS.init();
+
+var wave1 = $('#feel-the-wave').wavify({
+    height: 30,
+    bones: 5,
+    amplitude: 60,
+    color: '#29B2FF',
+    speed: .16
+});
+
+var wave2 = $('#feel-the-wave-two').wavify({
+    height: 30,
+    bones: 5,
+    amplitude: 40,
+    color: 'rgb(65, 65, 65)',
+    speed: .1
+});
