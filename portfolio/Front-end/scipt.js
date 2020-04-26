9dd76da2f381169d65ad12ac79601c6a8cb9eba4
datomezz <datomezz@gmail.com>
2020-04-26 06:59:40 +0400

    done with no responsive version

--- portfolio/Front-end/scipt.js
+++ portfolio/Front-end/scipt.js
@@ -1,5 +1,6 @@
 AOS.init();
 
+
 var wave1 = $('#feel-the-wave').wavify({
     height: 30,
     bones: 5,
@@ -15,3 +16,43 @@ var wave2 = $('#feel-the-wave-two').wavify({
     color: 'rgb(65, 65, 65)',
     speed: .1
 });
+
+$('.wrapper').slick({
+    lazyLoad: 'ondemand',
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    vertical : true,
+    verticalSwiping: true,
+    responsive : [
+        {
+            breakpoint : 768,
+            settings : {
+                arrows : false,
+                centerMode : true,
+                slidesToShow : 2
+            }
+        },
+        {
+            breakpoint : 500,
+            settings : {
+                arrows : false,
+                centerMode : true,
+                slidesToShow : 1
+            }
+
+        }
+    ]
+});
+
+$(".slick-next").text("").css({"display" : "none"});
+$(".slick-prev").text("").css({"display" : "none"});
+
+
+document.querySelectorAll(".section__arrow")[0].onclick = function(){
+    document.querySelector(".slick-prev").click();
+}
+document.querySelectorAll(".section__arrow")[1].onclick = function(){
+    document.querySelector(".slick-next").click();
+}
