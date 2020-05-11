a6505a6483bc48b1033ca7e3e77aeacb8a06c633
datomezz <datomezz@gmail.com>
2020-05-11 22:54:53 +0400

    php

---
+++ php/ajax.php
@@ -0,0 +1,7 @@
+<?php
+ $arr[] =  $_GET["name"];
+ $arr[] =  $_GET["lastName"];
+ $arr[] =  $_GET["number"];
+ $arr[] =  $_GET["boolean"];
+    echo $arr[2];
+?>
--- php/index.php
+++ php/index.php
@@ -8,8 +8,31 @@
 </head>
 
 <body>
+    <form>
+        <input type="text" name="name">
+        <input type="text" name="lastName">
+        <input type="text" name="number">
+        <input type="text" name="boolean">
+        <button type="submit" onclick="showText(this.value)">submit</button>
+        <h1>text</h1>
+    </form>
+
 
 <script>
+    function showText(val){
+        let req = new XMLHttpRequest();
+        req.onreadystatechange = function(){
+            if(this.readyState == 4 && this.status == 200){
+                console.log(this.response);
+            }
+        }
+        req.open("GET", "ajax.php?q=", true);
+        req.send();
+
+    }
+
+
+
     console.log("%cMEZZ.CODES %c\nFkn.. MEZZ.CODES .","font-size: 2rem; color: yellow; text-shadow: 1px 1px 1px black;","font-size: 12px;");
 </script>
 </body>
