6c46da2c1a8dd4885e1f1097c70bfb3b42e36aa8
datomezz <datomezz@gmail.com>
2020-05-14 00:00:29 +0400

    test

--- php/ajax.php
+++ php/ajax.php
@@ -1,7 +1,3 @@
-<?php
- $arr[] =  $_GET["name"];
- $arr[] =  $_GET["lastName"];
- $arr[] =  $_GET["number"];
- $arr[] =  $_GET["boolean"];
-    echo $arr[2];
-?>
+<div class="pizdec">
+    <p>Pizec</p>
+</div>
--- php/index.php
+++ php/index.php
@@ -5,31 +5,53 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mail Form</title>
+    <style>
+        .pizdec {
+            width : 20rem;
+            height : 15rem;
+            background : red;
+            animation-name : fade;
+            animation-duration : 2s;
+        }
+        @keyframes fade {
+            from { opacity : 0}
+            to {opacity : 1}
+        }
+    </style>
 </head>
 
 <body>
+<?php 
+
+    $connect = mysqli_connect('127.0.0.1', 'root', '');
+
+    if ($connect->connect_error) {
+        die("Connection failed: " . $connect->connect_error);
+    }
+    echo "Connected successfully";
+
+?>
     <form>
-        <input type="text" name="name">
-        <input type="text" name="lastName">
-        <input type="text" name="number">
-        <input type="text" name="boolean">
-        <button type="submit" onclick="showText(this.value)">submit</button>
+        <a href="javascript:void(0)" type="submit">submit</a>
         <h1>text</h1>
     </form>
 
 
 <script>
-    function showText(val){
+    const text = document.querySelector("h1");
+    const button = document.querySelector("a");
+
+    button.addEventListener("click", function(){
         let req = new XMLHttpRequest();
         req.onreadystatechange = function(){
             if(this.readyState == 4 && this.status == 200){
+                text.innerHTML = this.response;
                 console.log(this.response);
             }
         }
-        req.open("GET", "ajax.php?q=", true);
+        req.open("GET", "ajax.php", true);
         req.send();
-
-    }
+    });
 
 
 
