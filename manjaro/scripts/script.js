a13063292ab1a1ccd7a3d3ab1d34fdd0c6f3de16
datomezz <datomezz@gmail.com>
2020-06-17 19:25:01 +0400

    add Modal ajax function

--- manjaro/scripts/script.js
+++ manjaro/scripts/script.js
@@ -5,3 +5,30 @@ try {
 } catch (err) {
     console.log("tooltips Doesn't Exist");
 }
+
+let allCategory = document.querySelector(".header__category");
+let logIn = document.querySelectorAll(".user__login");
+
+logIn.forEach(item => {
+    item.addEventListener("click", function(){ 
+        modalAjax("Login Form", "testLogin.html");
+    });
+});
+
+allCategory.addEventListener("click", function() {
+    modalAjax("Login Form", "testLogin.html");
+});
+
+function modalAjax(header, url) {
+    let req = new XMLHttpRequest();
+    let modalHeader = document.querySelector(".modal-header");
+    let modalContent = document.querySelector(".modal-body");
+    req.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200){
+            modalHeader.innerText = header;
+            modalContent.innerHTML = this.response;
+        }
+    }
+    req.open("GET", url);
+    req.send();
+}
