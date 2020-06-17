58a5c34cdff8a368ed3c2aa359b89e9ecd7c277f
datomezz <datomezz@gmail.com>
2020-06-17 14:19:35 +0400

    add transitions on header

--- manjaro/scripts/script.js
+++ manjaro/scripts/script.js
@@ -1,6 +1,6 @@
 try {
     $("[data-toggle='tooltip']").tooltip({
-    template : '<div class="tooltip" role="tooltip"><div class="arrow"></div><div class="tooltip-inner"></div></div>' 
+        template : '<div class="tooltip" role="tooltip"><div class="arrow"></div><div class="tooltip-inner"></div></div>' 
     });
 } catch (err) {
     console.log("tooltips Doesn't Exist");
