3d0f2fd245c592b138e29a476ddd8802033efaaf
datomezz <datomezz@gmail.com>
2020-06-24 14:18:05 +0400

    add services

--- manjaro/scripts/script.js
+++ manjaro/scripts/script.js
@@ -40,17 +40,17 @@ function modalAjax(url, modalType) {
     req.send();
 }
 
-$('.slider__sale').slick({
+$('.slider-thumbnail').slick({
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
     dots : true,
-    dotsClass : "slider__sale-dots",
+    dotsClass : "slider-thumbnail__dots",
     prevArrow : "<a class='slider__arrow' href='javascript:void(0)'><i class='fas fa-arrow-left arrow'></i></a>",
     nextArrow : "<a class='slider__arrow' href='javascript:void(0)'><i class='fas fa-arrow-right arrow'></a>"
 });
 
-let sliderSaleDots = document.querySelectorAll(".slider__sale-dots button");
+let sliderSaleDots = document.querySelectorAll(".slider-thumbnail__dots button");
 
 if(sliderSaleDots.length > 0){
     for(let i = 0; i < sliderSaleDots.length; i++){
