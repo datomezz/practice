d51d8ee114967b89230eb57eb981ac9c6c6aead1
datomezz <datomezz@gmail.com>
2020-07-22 20:03:58 +0400

    practice with node js promises

--- nodejs/app.js
+++ nodejs/app.js
@@ -5,8 +5,8 @@ const mysql = require("mysql");
 const con = mysql.createConnection({
     host : "localhost",
     user : "root",
-    password : "",
-    database : "nodejs"
+    password : "datom3zz",
+    database : "zzD"
 });
 
 app.use(express.static("public"));
@@ -23,7 +23,7 @@ app.get("/", (req, res) => {
 app.get("/posts", (req, res) => {
     let posts = new Promise((resolve, reject) => {
         con.query(
-            "SELECT * FROM posts",
+            "SELECT * FROM nodejs",
             function(err, result) {
                 if(err) reject(err);
                 resolve(result);
@@ -49,7 +49,7 @@ app.get("/posts", (req, res) => {
 app.get("/post", (req, res) => {
     return new Promise((resolve, reject) => {
         con.query(
-            `SELECT * FROM posts WHERE id="${req.query.id}"`,
+            `SELECT * FROM nodejs WHERE id="${req.query.id}"`,
             function(err, result) {
                 if(err) reject(err);
                 resolve(result);
@@ -68,4 +68,35 @@ app.get("/post", (req, res) => {
     }).catch((err) => {
         console.log(err);
     })
+});
+
+app.get("/test", (req, res) => {
+    con.query(
+      "SELECT * FROM nodejs",
+      (err, result) => {
+          if(err) throw err;
+          let obj = {};
+          for(let i = 0; i < result.length; i++) {
+              obj[result[i]["id"]] = result[i];
+          }
+
+          res.render("test", {
+            obj : JSON.parse(JSON.stringify(obj))
+          });
+      }  
+    );
+});
+
+app.get("/burdul", (req, res) => {
+    let x = new Promise((resolve, reject) => {
+        setInterval(function(){
+            let rand = Math.floor(Math.random() * 1000);
+            resolve(rand);
+        }, 1000);
+    });
+    x.then((value) => {
+        res.render("burdul", {
+            rand : value
+        });
+    }).catch((err) => console.log(err));
 });
