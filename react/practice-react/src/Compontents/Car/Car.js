bc6b5b2d46d4c7c3264fe1ab72c05f9a826eb843
datomezz <datomezz@gmail.com>
2020-10-22 23:42:08 +0400

    practice react

--- react/practice-react/src/Compontents/Car/Car.js
+++ react/practice-react/src/Compontents/Car/Car.js
@@ -1,15 +1,19 @@
 import React from "react";
-import "./car.css";
+import "./car.scss";
 
 class Car extends React.Component {
   
   render() {
+    const inputArr = ["input"];
+
+    this.props.name !== "" ? inputArr.push("red") : inputArr.push("green");
+
     return (
       <div className={"car__container"}>
         <h3>{this.props.name}</h3>
         <h3>{this.props.year}</h3>
         <button onClick={this.props.onChangeTitle}>Click</button>
-        <input onChange={this.props.onChangeHandler} value={this.props.name} />
+        <input className={inputArr.join(" ")} onChange={this.props.onChangeHandler} value={this.props.name} />
         <button onClick={this.props.onDelete}>Delete</button>
       </div>
     )
