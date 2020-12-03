2d27d09fcd5e8bc91be76c9425d6e38dd68c16ce
datomezz <datomezz@gmail.com>
2020-12-04 00:20:01 +0400

    started slider

--- fedora/public/assets/scripts/main.js
+++ fedora/public/assets/scripts/main.js
@@ -1,1 +1,18 @@
 console.log("hellow");
+
+function changeTopSlider(e) {
+  const elementNumber = e.dataset.img;
+  
+  const allSliderForImgs = document.querySelectorAll(".slider-top__for > img");
+  const allSliderNavImgs = document.querySelectorAll(".slider-top__nav > img");
+
+  allSliderForImgs.forEach(item => {
+    item.classList.remove("slider__active");
+  });
+  allSliderNavImgs.forEach(item => {
+    item.classList.remove("slider__active");
+  });
+
+  allSliderNavImgs[elementNumber].classList.add("slider__active");  
+  allSliderForImgs[elementNumber].classList.add("slider__active");  
+}
